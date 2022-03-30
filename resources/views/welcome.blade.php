aa61c40aa62bf8633b342065761b0788c1dce4fc
Juan Asti <asti20001@gmail.com>
2022-03-30 13:45:46 -0500

    estilos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,19 +3,21 @@
 @section('cuerpo')
 
 <div class="container-fluid d-flex flex-column  align-items-center" style="height: 100vh">
-    <div class="row w-100 h-50">
+    <div class="row w-100">
         <div class="col d-flex justify-content-center align-items-center ">
-            <h1>Selecciona la denuncia</h1>
+            <div class="col-8">
+                <img class="img-fluid rounded" src="{{ asset('storage/img/Ilustración_pasos.jpg') }}" alt="">
+            </div>
         </div>
     </div>
-    <div class="row d-flex justify-content-center w-100 h-50">
+    <div class="row d-flex justify-content-center w-100">
         {{-- <div class="col-6 col-sm-4 col-md-3 text-center">
             <a href="{{route('denuncia-apropiacion-ilicita')}}" class="btn btn-outline-primary" style="font-size: 2rem">
                 Denuncia de Apropiacion Ilicita 
             </a>
         </div> --}}
         <div class="col-10 col-sm-8 col-md-6 text-center">
-            <a href="{{route('denuncia-violencia-familiar')}}" class="btn btn-outline-primary" style="font-size: 2rem">
+            <a href="{{route('denuncia-violencia-familiar')}}" role="button" class="btn btn-outline-primary" style="font-size: 1.5rem">
                 DENUNCIA POR DELITO DE VIOLENCIA FAMILIAR
             </a>
         </div>
